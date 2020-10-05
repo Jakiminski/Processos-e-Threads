5fb92d0af2da0484514cbbf7cc5512fef94ec8be
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2020-10-04 22:51:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from process import * # src process.py
 from util import * # src util.py
-from time import process_time_ns
+from time import time
 
 modo = 'multiprocessing' # 'multiprocessing' ou 'threading'
 processos = [] # Fila que guarda processos em execução
@@ -18,18 +18,15 @@ if __name__ == '__main__':
 	#printMatriz(matriz)
 	#print('Matriz gerada.')
 
-  matriz = criaMatriz(4)
-	printMatriz(matriz)
-	
 	if modo=='multiprocessing': # Executar processos concorrentes
 		
 		fila = Queue() # Fila com os valores de retorno
 		# Cria processos-flihos e os add na lista
 		n = len(matriz) # qnt de processos 
 		## Criar processos para cada rotina
-		processos = [ Process(name='worker{}'.format(i),target=pRoutine,args=(matriz[i],fila)) for i in range(n)]
+		processos = [Process(name='worker{}'.format(i),target=pRoutine,args=(matriz[i],fila)) for i in range(n)]
 		
-		pTempo = process_time_ns()
+		tempo = time()
 		print('Tempo inicial da CPU (ns): {}'.format(pTempo))
 
 		# Inicia a execução concorrente
@@ -40,14 +37,13 @@ if __name__ == '__main__':
 		for p in processos:
 			p.join()
       
-		fim = process_time_ns()
-
+		fim = time()
 		# ContaPrimos
 		for i in range(n):
 			contaPrimos += fila.get()
 		# Tempo final gasto pela CPU (não é tempo de relógio, considera apenas tempo em execução)
-		print('Tempo final da CPU: {}'.format(pTempo))
-		print('Intervalo de execução {} ns.'.format(fim - pTempo))
+		print('Tempo final da CPU: {}'.format(tempo))
+		print('Intervalo de execução {} ns.'.format(fim - tempo))
 		print('{} num primos encontrados.'.format(contaPrimos))
 		
 	elif modo=='threading': # # Executar threads
