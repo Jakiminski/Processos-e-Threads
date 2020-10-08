3079967ce307aedd0e3478525b039ad81ee7f00c
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2020-10-08 02:07:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -87,30 +87,3 @@ if globals.modo=='threading' or globals.modo=='both': # # Executar threads
 	#print('Tempo final da CPU: {}'.format(tempo))
 	
 	print('Intervalo de execução {}.'.format(fim - tempo))
-
-######################################################
-######################################################
-######################################################
-######################################################
-######################################################
-"""
-import threading
-
-def calc_square(number):
-    print('Square:' , number * number)
-def calc_quad():
-    print('Quad:' , number * number * number * number)
-if __name__ == "__main__":
-    number = 7
-    thread1 = threading.Thread(target=calc_square, args=(number,))
-    thread2 = threading.Thread(target=calc_quad, args=())
-    # Will execute both in parallel
-    thread1.start()
-    thread2.start()
-    # Joins threads back to the parent process, which is this
-    # program
-    thread1.join()
-    thread2.join()
-
-# This program reduces the time of execution by running tasks in parallel
-"""
